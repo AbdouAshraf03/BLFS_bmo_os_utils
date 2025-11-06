58350c0252b8024906cf990498accbdda929ed48
Abdou Ashraf <76920362+AbdouAshraf03@users.noreply.github.com>
2025-11-07 00:21:43 +0200

    Add libtasn1 installation script
    This script checks for the existence of a folder with the same name as the script. If the folder does not exist, it runs an installer, configures, builds, and installs the necessary files.

---
+++ scripts/General Libraries/libtasn1-4.20.0.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+
+# Get the script name without .sh extension
+folder_name=$(basename "$0" .sh)
+
+# Check if a folder with the same name exists
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+else
+    . ./../instller.sh
+
+    # configure 
+    ./configure --prefix=/usr --disable-static 
+
+    # make it 
+    make
+
+    # install it 
+    make install
+
+    # If you did not pass the --enable-gtk-doc parameter to the configure script, you can install the API documentation using the following command as the root user:
+    make -C doc/reference install-data-local
+    
+fi
