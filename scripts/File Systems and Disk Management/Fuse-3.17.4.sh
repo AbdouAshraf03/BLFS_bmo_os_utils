cc75ce97d00299b8f560b87c75e1258463e84b95
mohamed <elsharifmohamed577@gmail.com>
2025-11-12 04:00:05 +0200

    Add configuration for FUSE mounts in /etc/fuse.conf

--- scripts/File Systems and Disk Management/Fuse-3.17.4.sh
+++ scripts/File Systems and Disk Management/Fuse-3.17.4.sh
@@ -46,6 +46,17 @@ else
     install -v -m644   doc/{README.NFS,kernel.txt} \
                     /usr/share/doc/fuse-3.17.4
 
+    cat > /etc/fuse.conf << "EOF"
+    # Set the maximum number of FUSE mounts allowed to non-root users.
+    # The default is 1000.
+    #
+    #mount_max = 1000
+
+    # Allow non-root users to specify the 'allow_other' or 'allow_root'
+    # mount options.
+    #
+    #user_allow_other
+    EOF
 fi
 
 
