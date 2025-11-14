b7caf5fcd305d2b292cb5c31c51e345b99b5fe7d
abdou <abdouashrat123@gmail.com>
2025-11-14 17:45:24 +0200

    Add script for downloading and building Abseil C++ library

---
+++ scripts/General Libraries/Abseil-cpp-20250814.0.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://github.com/abseil/abseil-cpp/releases/download/20250814.0/abseil-cpp-20250814.0.tar.gz
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+   mkdir build &&
+   cd    build
+
+   echo "🔧 Running configure..."
+    if ! cmake -D CMAKE_INSTALL_PREFIX=/usr \
+      -D CMAKE_BUILD_TYPE=Release  \
+      -D ABSL_PROPAGATE_CXX_STD=ON \
+      -D BUILD_SHARED_LIBS=ON      \
+      -G Ninja ..             ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
