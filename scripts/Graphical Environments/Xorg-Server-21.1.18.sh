9b2ce325a33a81adfc742ce8a4103560ba49a867
abdou <abdomashal980@gmail.com>
2025-11-19 05:08:38 +0200

    Add installation scripts for various Xorg input drivers and utilities

---
+++ scripts/Graphical Environments/Xorg-Server-21.1.18.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    wget  https://www.linuxfromscratch.org/patches/blfs/12.4/xorg-server-21.1.18-tearfree_backport-1.patch --no-check-certificate
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://www.x.org/pub/individual/xserver/xorg-server-21.1.18.tar.xz
+    echo "✅ the package downloaded successfully"
+
+   patch -Np1 -i ../xorg-server-21.1.18-tearfree_backport-1.patch
+
+   mkdir build &&
+    cd    build
+
+   echo "🔧 Running configure..."
+    if ! meson setup ..              \
+            --prefix=$XORG_PREFIX \
+            --localstatedir=/var  \
+            -D glamor=true        \
+            -D xkb_output_dir=/var/lib/xkb; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! ninja; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! ninja install>; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   mkdir -pv /etc/X11/xorg.conf.d
+
+fi
+
+
+echo "🎉 FINISHED :)"
---
+++ scripts/Graphical Environments/xterm-401.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh https://invisible-mirror.net/archives/xterm/xterm-401.tgz
+    echo "✅ the package downloaded successfully"
+
+   sed -i '/v0/{n;s/new:/new:kb=^?:/}' termcap &&
+    printf '\tkbs=\\177,\n' >> terminfo &&
+
+    TERMINFO=/usr/share/terminfo \
+
+   echo "🔧 Running configure..."
+    if ! ./configure $XORG_CONFIG     \
+                    --with-app-defaults=/etc/X11/app-defaults; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+    mkdir -pv /usr/share/applications &&
+    cp -v *.desktop /usr/share/applications/
+
+    cat >> /etc/X11/app-defaults/XTerm << "EOF"
+*VT100*locale: true
+*VT100*faceName: Monospace
+*VT100*faceSize: 10
+*backarrowKeyIsErase: true
+*ptyInitialErase: true
+EOF
+
+fi
+
+
+echo "🎉 FINISHED :)"
