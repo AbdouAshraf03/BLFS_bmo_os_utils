17c27bf3d38d7567a994c473b593974ab32b3cfa
WaleedEmad173 <waleed.emad173@gmail.com>
2025-11-25 22:42:11 +0200

    Add libtirpc installation script version 1.3.6 with build and installation steps

---
+++ scripts/Networking Libraries/libtirpc-1.3.6.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cd ~/sources/BLFS || exit 1
+
+folder_name=$(basename "$0" .sh)
+
+# Convert to lowercase
+folder_name=$(echo "$folder_name" | tr '[:upper:]' '[:lower:]')
+
+if [ -d "$folder_name" ]; then
+    echo "✅ Folder '$folder_name' exists."
+    exit 1
+else
+    . ./../BLFS_bmo_os_utils/scripts/installer.sh  https://downloads.sourceforge.net/libtirpc/libtirpc-1.3.6.tar.bz2
+    echo "✅ the package downloaded successfully"
+
+   # <MORE_COMMAND_IF_EXISTS_WITH_IF_STATEMENT>
+   patch -Np1 -i ../libtirpc-1.3.6-gcc15_fixes-1.patch
+
+   echo "🔧 Running configure..."
+    if ! ./configure --prefix=/usr     \
+            --sysconfdir=/etc \
+            --disable-static  \
+            --disable-gssapi  ; then
+        echo "❌ Error: configure failed!"
+        exit 1
+    fi
+
+    echo "⚙️  Running make..."
+    if ! make; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+    
+    echo "⚙️ installing..."
+    if ! make install; then
+        echo "❌ Error: make failed!"
+        exit 1
+    fi
+
+   # <ETC>
+
+fi
+
+
+echo "🎉 FINISHED :)"
